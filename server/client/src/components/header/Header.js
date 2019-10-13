df3d080a628327a5ccf51e5cef4dfa41c4c8cf03
Alexander Kayuda <alex.kaes@gmail.com>
2019-10-12 23:35:24 -0400

    [Client] Minor Update

--- server/client/src/components/header/Header.js
+++ server/client/src/components/header/Header.js
@@ -28,9 +28,7 @@ class Header extends Component {
                     <div className="collapse navbar-collapse" id="mobile-nav">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item">
-                                <a className="nav-link" href="/dashboard">
-                                    Dashboard
-                                </a>
+                                <Link to={'/dashboard'} className="nav-link">Dashboard</Link>
                             </li>
                         </ul>
                         <ul className="navbar-nav ml-auto">
